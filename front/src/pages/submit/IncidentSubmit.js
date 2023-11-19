799c8d4ebd354b9d27c33202381a0bd97f3d6f9a
George <gkkobakhidze@gmail.com>
2023-11-19 05:44:28 +0300

    added some more stuff for TrAnsAcTiOn AnAlySis

--- front/src/pages/submit/IncidentSubmit.js
+++ front/src/pages/submit/IncidentSubmit.js
@@ -1,7 +1,7 @@
 // src/pages/index.js
 
 import { Stack, Typography } from '@mui/material';
-
+import { cleanData } from './scripts';
 import { TextField, Button, Box } from '@mui/material';
 import axios from 'axios';
 import React from 'react';
@@ -25,23 +25,41 @@ export class IncidentSubmit extends React.Component {
   
   
   handleSubmit = async (event) => {
-    event.preventDefault(); // Prevent default form submission behavior
-    const pb = new PocketBase('http://127.0.0.1:8090');
 
-    const { tx, method_of_hack, related_domain } = this.state;
+    event.preventDefault();
+    let modifiedTx = this.state.tx.substring(2);
 
-    try {
+    // API URL with the modified transaction ID
+    const apiUrl = `https://api.blockcypher.com/v1/eth/main/txs/${modifiedTx}`;
 
-      const record = await this.client.collection('incidents').create({
-        tx: tx,
-        method_of_hack: method_of_hack,
-        related_domain: related_domain
-      });
+    // Making a GET request to the API
+    axios.get(apiUrl)
+        .then( response  => {
+            // Output the response data to the console
+            console.log(response.data);
+            const pb = new PocketBase('http://127.0.0.1:8090');
 
-      console.log('Record created:', record);
-    } catch (error) {
-      console.error('Error submitting incident:', error);
-    }
+            const { method_of_hack, related_domain } = this.state;
+
+            try {
+              const record = this.client.collection('incidents').create({
+                tx: this.state.tx,
+                method_of_hack: method_of_hack,
+                related_domain: related_domain,
+                sender:`0x${response.data.addresses[0]}`,
+                recipient:`0x${response.data.addresses[1]}`,
+              });
+
+              console.log('Record created:', record);
+            } catch (error) {
+              console.error('Error submitting incident:', error);
+            }
+        })
+        .catch(error => {
+            // Handle any errors here
+            console.error('Error fetching data:', error);
+        });
+    
   };
 
   handleChange = (e) => {
--- front/src/pages/submit/scripts.js
+++ front/src/pages/submit/scripts.js
@@ -1,4 +1,23 @@
-import axios from 'axios';
-export function cleanData(tx, hack_method, related_domain){
-    
+import axios from "axios";
+
+export function cleanData(tx){
+
+    // Remove the first two characters ('0x') from the transaction ID
+    let modifiedTx = tx.substring(2);
+
+    // API URL with the modified transaction ID
+    const apiUrl = `https://api.blockcypher.com/v1/eth/main/txs/${modifiedTx}`;
+
+    // Making a GET request to the API
+    axios.get(apiUrl)
+        .then(response => {
+            // Output the response data to the console
+            console.log(response.data);
+        })
+        .catch(error => {
+            // Handle any errors here
+            console.error('Error fetching data:', error);
+        });
+    return tx;
+
 }
